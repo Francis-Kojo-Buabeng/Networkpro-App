8bd69d7894af3fe1927667f0258ceabe4ee2a6aa
Francis Kojo Buabeng <buabengf9@gmail.com>
2025-07-14 15:03:15 +0000

    commit all changed files

--- Networkpro/Backend/auth-service/src/main/java/com/networkpro/NetworkproApplication.java
+++ Networkpro/Backend/auth-service/src/main/java/com/networkpro/NetworkproApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @SpringBootApplication
 public class NetworkproApplication {
@@ -14,14 +15,13 @@ public class NetworkproApplication {
 		SpringApplication.run(NetworkproApplication.class, args);
 	}
 
-<<<<<<< HEAD:Networkpro/Backend/auth-service/src/main/java/com/networkpro/LinkedInApplication.java
 	@Configuration
 	public class WebConfig {
 		@Bean
 		public WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurer() {
 				@Override
-				public void addCorsMappings(CorsRegistry registry) {
+				public void addCorsMappings(@NonNull CorsRegistry registry) {
 					registry.addMapping("/**")
 							.allowedOrigins("*") // Or specify your frontend URL
 							.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
@@ -30,6 +30,4 @@ public class NetworkproApplication {
 			};
 		}
 	}
-=======
->>>>>>> d0f9877a67400cd0cf364fb90e78c660c8eb5486:Networkpro/Backend/auth-service/src/main/java/com/networkpro/NetworkproApplication.java
 }
