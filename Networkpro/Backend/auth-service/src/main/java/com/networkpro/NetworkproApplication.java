d0f9877a67400cd0cf364fb90e78c660c8eb5486
Francis Kojo Buabeng <buabengf9@gmail.com>
2025-07-14 14:13:36 +0000

    updated the application.properties, .yml, dockerfile

--- Networkpro/Backend/auth-service/src/main/java/com/networkpro/NetworkproApplication.java
+++ Networkpro/Backend/auth-service/src/main/java/com/networkpro/NetworkproApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkedInApplication {
+public class NetworkproApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LinkedInApplication.class, args);
+		SpringApplication.run(NetworkproApplication.class, args);
 	}
-
 
 }
